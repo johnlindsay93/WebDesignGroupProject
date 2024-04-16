276fa095dc88d7b264e685954e10d46910883bdf
John Lindsay <johnlindsay@johns-air.home>
2024-04-16 16:24:29 +0100

    Adding form page with necessary javascript

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Javascript to create a pop up box with information text on each cruise
 $(document).ready(function() {
     // Attach click event to images with class "image-popup"
     $(".image-popup").click(function() {
@@ -11,7 +12,59 @@ $(document).ready(function() {
     // Attach click event to the button inside the modal
     $("#popup-button").click(function() {
     // Redirect to form.html with query parameter
-    window.location.href = "form.html?prepopulate=" + encodeURIComponent($("#popup-text").text());
+    window.location.href = "form.html"
     });
 });
-    
+
+
+//javascript to validate form input
+const email = document.getElementById('email')
+const username = document.getElementById('name')
+const form = document.getElementById('booking-form')
+const number = document.getElementById('number')
+const errorElement = document.getElementById('error')
+const dropdown = document.getElementById('cruise')
+const radioButton = document.querySelectorAll('input[type="radio"]')
+const checkbox = document.getElementById('flexCheckDefault')
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+form.addEventListener('submit', (e) => {
+    // Prevent default behaviour on submit
+    e.preventDefault()
+    let messages = []
+
+    // validate there is a name enterred
+    if (username.value === '' || username.value == null){
+        messages.push('Your Name is Required')
+    }
+
+    //remove whitespacing from email address
+    var nowhitespace = email.value.trim()
+    // validate that a valid email address is entered 
+    if(!emailRegex.test(nowhitespace)){
+        messages.push('You must enter a valid email address')
+    }
+
+    if (isNaN(number)){
+        messages.push('Please enter a valid number')
+    }
+
+    var selectedOption = dropdown.value;
+    if (selectedOption === '') {
+        messages.push('Please select a cruise')
+    }
+
+    var checked = Array.from(radioButton).some(radio => radio.checked);
+    if (!checked) {
+        messages.push('Please choose yes or no')
+    }
+
+    if (!checkbox.checked){
+        messages.push('Please confirm your information')
+    }
+
+    if (messages.length > 0){
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+    }
+})
